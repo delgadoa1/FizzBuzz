424f2ca62be3df383d8ce8f492e366be2bccd4cb
Andres Delgado <NJandreNJ@gmail.com>
2017-11-29 17:17:29 -0500

    First Commit

---
+++ True Fizzbuzz.py
@@ -0,0 +1,9 @@
+for n in range(1, 101):
+    if n % 3 == 0 and n % 5 ==0:
+        print(n, "FizzBuzz")
+    elif n % 3 == 0:
+        print(n, "Fizz")
+    elif n % 5 == 0:
+        print(n, "Buzz")
+    else:
+        print(n)
